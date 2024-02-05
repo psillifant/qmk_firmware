10e9c4a8869dd7196aa163812ec48aeb836fc5f5
P Sillifant <peter.sillifant@gmail.com>
2024-02-05 21:14:05 +0000

    Added LT to layers one, two and three, so that tapping is different to holding.

--- keyboards/planck/keymaps/dblspc_v0.1/keymap.c
+++ keyboards/planck/keymaps/dblspc_v0.1/keymap.c
@@ -16,20 +16,21 @@
 
 #include QMK_KEYBOARD_H
 
-#ifdef AUDIO_ENABLE
-#    include "muse.h"
-#endif
+// #ifdef AUDIO_ENABLE
+// #    include "muse.h"
+// #endif
 
 enum planck_layers {
   _QWERTYPC,
   _QWERTYMAC,
   _NUMBERS,
   _SYMBOLS,
-  _LYRONE,
-  _LYRTWO,
-  // _GAMES,
-  // _LAYER_INDEX,
-  _BOOTLD
+  _EMOJI,
+  _MEDIA,
+  _MOVE,
+  _GAMES,
+  _BOOTLD,
+  _LAYERNAVIGATION
 };
 
 enum planck_keycodes {
@@ -38,15 +39,16 @@ enum planck_keycodes {
 };
 
 #define TG_QWER TO(_QWERTYPC)
-// #define TG_GAMS TO(_GAMES)
+#define TG_GAMS TO(_GAMES)
 #define TG_PC TO(_QWERTYPC)
 #define TG_MAC TO(_QWERTYMAC)
-#define LY_ONE MO(_LYRONE)
-#define LY_TWO MO(_LYRTWO)
+#define ME_PIPE S(KC_NUBS)
+#define LY_MED LT(_MEDIA, ME_PIPE)
+#define LY_MOVE LT(_MOVE, KC_BSLS)
 #define LY_GAMS MO(_GAMES)
-// #define LY_INDX MO(_LAYER_INDEX)
 #define LY_SYMB MO(_SYMBOLS)
 #define LY_NUMS MO(_NUMBERS)
+#define LT_EMOJ LT(_EMOJI, KC_MINS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -54,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_GRAVE,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,
-    KC_LCTL,   KC_LGUI,  KC_LALT,  LY_SYMB,  KC_SPC,             KC_SPC,             LY_NUMS,  KC_NO,    LY_ONE,   LY_TWO
+    KC_LCTL,   KC_LGUI,  KC_LALT,  LY_SYMB,  KC_SPC,             KC_SPC,             LY_NUMS,  LT_EMOJ,  LY_MED,   LY_MOVE
 ),
 
 [_QWERTYMAC] = LAYOUT_planck_2x2u(
     KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_GRAVE,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,
-    KC_LCTL,   KC_LALT,  KC_LGUI,  LY_SYMB,  KC_SPC,             KC_SPC,             LY_NUMS,  KC_NO,    LY_ONE,   LY_TWO
+    KC_LCTL,   KC_LALT,  KC_LGUI,  LY_SYMB,  KC_SPC,             KC_SPC,             LY_NUMS,  LT_EMOJ,  LY_MED,   LY_MOVE
 ),
 
 [_SYMBOLS] = LAYOUT_planck_2x2u(
@@ -78,42 +80,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,   _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______
 ),
 
-// [_LAYER_INDEX] = LAYOUT_planck_2x2u(
-//     KC_NO,     TG_QWER,  TG_GAMS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-//     KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-//     _______,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-//     _______,   _______,  _______,  _______,  _______,            _______,            _______,  LY_INDX,  _______,  _______
-// ),
-
-[_LYRONE] = LAYOUT_planck_2x2u(
-    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_VOLU,
-    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_VOLD,
-    _______,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MUTE,  KC_NO,    KC_NO,    KC_MUTE,
+// LAYER KEY ONE
+[_EMOJI] = LAYOUT_planck_2x2u(
+    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    _______,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     _______,   _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______
 ),
 
-[_LYRTWO] = LAYOUT_planck_2x2u(
-    KC_ESC,    KC_NO,    KC_UP,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_DEL,
+// LAYER KEY TWO
+[_MEDIA] = LAYOUT_planck_2x2u(
+    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MPRV,  KC_VOLU,
+    KC_NO,     KC_NO,    KC_MSTP,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_VOLD,
+    _______,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MNXT,  KC_NO,    KC_MUTE,  KC_NO,    KC_NO,    KC_MUTE,
+    _______,   _______,  _______,  _______,  KC_MPLY,            KC_MPLY,            _______,  _______,  _______,  _______
+),
+// LAYER KEY THREE
+[_MOVE] = LAYOUT_planck_2x2u(
+    KC_ESC,    KC_HOME,  KC_UP,    KC_END,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_DEL,
     KC_NO,     KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_END,   KC_NO,
     KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
     _______,   _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______
 ),
 
-// [_GAMES] = LAYOUT_planck_2x2u(
-//     KC_NO,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_NO,
-//     KC_NO,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-//     _______,   KC_A,     KC_S,     KC_D,     KC_F,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-//     KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_SPC,             _______,            _______,  LY_INDX,  _______,  _______
-// ),
+[_GAMES] = LAYOUT_planck_2x2u(
+    KC_ESC,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_NO,
+    KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    _______,   KC_A,     KC_S,     KC_D,     KC_F,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_SPC,             _______,            _______,  _______,  _______,  _______
+),
 
 [_BOOTLD] = LAYOUT_planck_2x2u(
-    QK_BOOT,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    TG_PC,
-    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    TG_MAC,
+    QK_BOOT,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_CAPS,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     _______,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     _______,   _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______
 ),
 
-
+[_LAYERNAVIGATION] = LAYOUT_planck_2x2u(
+    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    TG_PC,
+    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    TG_MAC,
+    _______,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    TG_GAMS,
+    _______,   _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______
+),
 
 // [_BLANK] = LAYOUT_planck_2x2u(
     // KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
@@ -126,6 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   state = update_tri_layer_state(state, _SYMBOLS, _NUMBERS, _BOOTLD);
+  state = update_tri_layer_state(state, _MEDIA, _MOVE, _LAYERNAVIGATION);
   switch (get_highest_layer(state)) {
     case _QWERTYPC:
         rgblight_setrgb (RGB_WHITE);
